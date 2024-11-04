d5dcdd15a8dbcbdf5dbcfafef411e642fd835831
SolanaSpinDev3 <dev_sol_99@proton.me>
2024-11-04 17:25:21 +0200

    optimize child components load

--- src/apps/nextjs/app/components/PrizeAnnouncement/index.tsx
+++ src/apps/nextjs/app/components/PrizeAnnouncement/index.tsx
@@ -2,43 +2,45 @@ import React, {useEffect, useState} from 'react';
 import confetti from 'canvas-confetti';
 import './PrizeAnnouncement.css'
 import {NauSea} from "@/app/fonts/fonts";
+import {Balance} from "@/app/components/Balance";
 
-const PrizeAnnouncement = ({hasWon, message, onAnimationComplete}) => {
-    const [isVisible, setIsVisible] = useState(false);
+const PrizeAnnouncement = React.memo(
+    ({hasWon, message, onAnimationComplete}: { hasWon: boolean, message: string, onAnimationComplete: () => void }) => {
+        const [isVisible, setIsVisible] = useState(false);
+        console.log('prize is announced')
+        const triggerConfetti = () => {
+            confetti({
+                particleCount: 150,
+                spread: 80,
+                origin: {y: 0.6},
+            });
+        };
+        useEffect(() => {
+            if (hasWon) {
+                setIsVisible(true);
+            }
+        }, [hasWon]);
 
-    const triggerConfetti = () => {
-        confetti({
-            particleCount: 150,
-            spread: 80,
-            origin: {y: 0.6},
-        });
-    };
-    useEffect(() => {
-        if (hasWon) {
-            setIsVisible(true);
-        }
-    }, [hasWon]);
+        useEffect(() => {
+            if (hasWon) {
+                triggerConfetti();
+            }
+        }, [hasWon]);
 
-    useEffect(() => {
-        if (hasWon) {
-            triggerConfetti();
-        }
-    }, [hasWon]);
+        const handleAnimationEnd = () => {
+            setIsVisible(false);
+            onAnimationComplete()
+        };
 
-    const handleAnimationEnd = () => {
-        setIsVisible(false);
-        onAnimationComplete()
-    };
-
-    return (
-        <>
-            {isVisible && hasWon && <div className="absolute bg-black/75 w-full h-full block z-50">
-                <div className={`z-30 text-red-900 prize-display`}  onAnimationEnd={handleAnimationEnd}>
-                    <h1 className={`${NauSea.className} tracking-[3px] font-thin`}>{message}</h1>
-                </div>
-            </div>}
-        </>
-    );
-};
-
+        return (
+            <>
+                {isVisible && hasWon && <div className="absolute bg-black/75 w-full h-full block z-50">
+                    <div className={`z-30 text-red-900 prize-display`} onAnimationEnd={handleAnimationEnd}>
+                        <h1 className={`${NauSea.className} tracking-[3px] font-thin`}>{message}</h1>
+                    </div>
+                </div>}
+            </>
+        );
+    });
+PrizeAnnouncement.displayName = 'PrizeAnnouncement';
 export default PrizeAnnouncement;
