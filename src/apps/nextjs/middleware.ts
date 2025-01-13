2ce35fc1b3002aa175f5f3866567983507b24018
SolanaSpinDev3 <dev_sol_99@proton.me>
2025-01-13 18:18:10 +0200

    temporary disable middleware

--- src/apps/nextjs/middleware.ts
+++ src/apps/nextjs/middleware.ts
@@ -8,33 +8,33 @@ import {NextRequest, NextResponse} from "next/server";
 // });
 
 
-export function middleware(request: NextRequest) {
-    const url = request.nextUrl;
-    console.log('@Middleware.ts');
-    //1. Exclude NextAuth API routes
-    if (url.pathname.startsWith("/api/auth/")) {
-        return NextResponse.next();
-    }
-
-    if (url.pathname.startsWith("/api/users/self-register")) {
-        return NextResponse.next();
-    }
-    // 2. Redirect root path "/" to default game mode
-    if (url.pathname === "/") {
-        return NextResponse.redirect(new URL("/game/default", request.url));
-    }
-
-    // Add custom logic for protected routes if needed
-    const isProtectedRoute = url.pathname.startsWith("/protected/");
-    const token = request.cookies.get("next-auth.session-token");
-
-    if (isProtectedRoute && !token) {
-        return NextResponse.redirect(new URL("/auth/signin", request.url));
-    }
-
-    return NextResponse.next();
-}
-
-export const config = {
-    matcher: ["/protected/:path*", "/api/auth/:path*"], // Ensure /api/auth routes are handled
-};
+// export function middleware(request: NextRequest) {
+//     const url = request.nextUrl;
+//     console.log('@Middleware.ts');
+//     //1. Exclude NextAuth API routes
+//     if (url.pathname.startsWith("/api/auth/")) {
+//         return NextResponse.next();
+//     }
+//
+//     if (url.pathname.startsWith("/api/users/self-register")) {
+//         return NextResponse.next();
+//     }
+//     // 2. Redirect root path "/" to default game mode
+//     if (url.pathname === "/") {
+//         return NextResponse.redirect(new URL("/game/default", request.url));
+//     }
+//
+//     // Add custom logic for protected routes if needed
+//     const isProtectedRoute = url.pathname.startsWith("/protected/");
+//     const token = request.cookies.get("next-auth.session-token");
+//
+//     if (isProtectedRoute && !token) {
+//         return NextResponse.redirect(new URL("/auth/signin", request.url));
+//     }
+//
+//     return NextResponse.next();
+// }
+//
+// export const config = {
+//     matcher: ["/protected/:path*", "/api/auth/:path*"], // Ensure /api/auth routes are handled
+// };
