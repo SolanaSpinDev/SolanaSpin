a3545aa9cc2372eb02f2676e6e9dc1064d1cbe52
SolanaSpinDev3 <dev_sol_99@proton.me>
2025-01-15 13:54:31 +0200

    update useBalanceData.ts witht he right get profile data

--- src/apps/nextjs/app/hooks/useBalanceData.ts
+++ src/apps/nextjs/app/hooks/useBalanceData.ts
@@ -1,14 +1,30 @@
 import useSWR from 'swr';
-import {fetcher} from '@/lib/fetcher';
 import {useBalance} from '@/app/context/BalanceContext';
 import React from "react";
+import { useSession } from 'next-auth/react';
+import {fetchWithAuth} from "@/app/api/utils/api";
 
 export const useBalanceData = () => {
-    const {data, error, mutate} = useSWR('/api/getProfile', fetcher); //todo add the right method
     const {setBalance} = useBalance();
+    const { data: session } = useSession();
+
+    const fetcher = async (url: string, token: string) => {
+        if (!token) {
+            throw new Error('No authentication token found');
+        }
+
+        const data = await fetchWithAuth(url, 'GET', token);
+        return data;
+    };
+
+    const { data, error, mutate } = useSWR(
+        session && session.tokens?.token ? ['/api/users/profile', session.tokens.token] : null,
+        fetcher
+    );
 
     React.useEffect(() => {
         if (data && data.balance !== undefined) {
+            console.log('Fetched balance:', data.balance);
             setBalance(data.balance);
         }
     }, [data, setBalance]);
